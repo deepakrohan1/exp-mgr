f6d78178651ca8a83fe6cca279d7ed18cf7402e6
rohan <deepakrohan@hotmail.com>
2021-09-30 16:50:07 -0400

    adding the categories entity close #7

--- src/main/java/com/deepakrohan/expense/service/CategoryService.java
+++ src/main/java/com/deepakrohan/expense/service/CategoryService.java
@@ -4,11 +4,9 @@ import com.deepakrohan.expense.dto.CategoryDto;
 import com.deepakrohan.expense.entity.Category;
 import com.deepakrohan.expense.repo.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class CategoryService {
