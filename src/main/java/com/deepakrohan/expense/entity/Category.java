d920213eb7994900c72d251e6aa4de07dc087e6b
rohan <deepakrohan@hotmail.com>
2021-09-29 21:51:49 -0400

    circle ci updates added

--- src/main/java/com/deepakrohan/expense/entity/Category.java
+++ src/main/java/com/deepakrohan/expense/entity/Category.java
@@ -1,66 +1,34 @@
 package com.deepakrohan.expense.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.time.LocalDateTime;
 import java.util.List;
 
+//@AllArgsConstructor
+//@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@ToString
+@Entity
 public class Category {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String expenseCat;
     private String expenseCatDesc;
     private List<ExpenseItem> listOfExpenses;
     private LocalDateTime timeCreated;
-
-    public Category() {}
-
-    public Category(String id, String expenseCat, String expenseCatDesc, List<ExpenseItem> listOfExpenses, LocalDateTime timeCreated) {
-        this.id = id;
-        this.expenseCat = expenseCat;
-        this.expenseCatDesc = expenseCatDesc;
-        this.listOfExpenses = listOfExpenses;
-        this.timeCreated = timeCreated;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getExpenseCat() {
-        return expenseCat;
-    }
-
-    public void setExpenseCat(String expenseCat) {
-        this.expenseCat = expenseCat;
-    }
-
-    public String getExpenseCatDesc() {
-        return expenseCatDesc;
-    }
-
-    public void setExpenseCatDesc(String expenseCatDesc) {
-        this.expenseCatDesc = expenseCatDesc;
-    }
-
-    public List<ExpenseItem> getListOfExpenses() {
-        return listOfExpenses;
-    }
-
-    public void setListOfExpenses(List<ExpenseItem> listOfExpenses) {
-        this.listOfExpenses = listOfExpenses;
-    }
-
-    public LocalDateTime getTimeCreated() {
-        return timeCreated;
-    }
-
-    public void setTimeCreated(LocalDateTime timeCreated) {
-        this.timeCreated = timeCreated;
-    }
 }
+
--- src/main/java/com/deepakrohan/expense/repo/CategoryRepository.java
+++ src/main/java/com/deepakrohan/expense/repo/CategoryRepository.java
@@ -1,8 +1,9 @@
 package com.deepakrohan.expense.repo;
 
 import com.deepakrohan.expense.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CategoryRepository  {
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
 }
