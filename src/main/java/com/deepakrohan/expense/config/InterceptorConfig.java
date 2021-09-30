071725a38599dc13f41a5f9c537c3a75dc09b93d
rohan <deepakrohan@hotmail.com>
2021-09-29 22:28:31 -0400

    lombok dependencies issue

--- src/main/java/com/deepakrohan/expense/config/InterceptorConfig.java
+++ src/main/java/com/deepakrohan/expense/config/InterceptorConfig.java
@@ -1,14 +1,5 @@
 package com.deepakrohan.expense.config;
 
-import com.deepakrohan.expense.interceptors.ReqInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@Component
 public class InterceptorConfig {
 
 
