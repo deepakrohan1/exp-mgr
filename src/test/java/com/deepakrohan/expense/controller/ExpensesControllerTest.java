0440091838108657c58ad07bb9d504de8396bafd
rohan <deepakrohan@hotmail.com>
2021-09-29 22:28:12 -0400

    test cases issue fixed

--- src/test/java/com/deepakrohan/expense/controller/ExpensesControllerTest.java
+++ src/test/java/com/deepakrohan/expense/controller/ExpensesControllerTest.java
@@ -1,11 +1,10 @@
 package com.deepakrohan.expense.controller;
 
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,42 +17,7 @@ import java.util.Arrays;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(ExpensesController.class)
+@SpringBootTest
 public class ExpensesControllerTest {
-
-    @Autowired
-    public MockMvc mockMvc;
-
-    @Autowired
-    NumberFormat nf;
-
-    @Autowired
-    ApplicationContext ac;
-
-    @Before
-    public void preSetup() {
-        int count = ac.getBeanDefinitionCount();
-        System.out.println(String.format("Number of Beans %d" , count));
-        String[] names = ac.getBeanDefinitionNames();
-        Arrays.stream(names)
-                .sorted()
-                .forEach(System.out::println);
-    }
-
-    @Test
-    public void test_expensesControllerGetMethod() throws Exception {
-        mockMvc.perform( MockMvcRequestBuilders
-                .get("/v1/expenses")
-                .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void numberFormatterTest() {
-        double amount = 1324334.1232233;
-        System.out.println(nf.format(amount));
-    }
 
 }
