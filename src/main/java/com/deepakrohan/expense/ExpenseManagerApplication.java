fdb02ee6b7d607dca74e058cef053482532567d0
rohan <deepakrohan@hotmail.com>
2021-10-03 19:24:27 -0400

    implementing i18n for messages

--- src/main/java/com/deepakrohan/expense/ExpenseManagerApplication.java
+++ src/main/java/com/deepakrohan/expense/ExpenseManagerApplication.java
@@ -1,11 +1,15 @@
 package com.deepakrohan.expense;
 
+import org.hibernate.validator.spi.messageinterpolation.LocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import javax.servlet.annotation.WebServlet;
+import java.util.Locale;
 
 @SpringBootApplication
 public class ExpenseManagerApplication {
@@ -14,6 +18,20 @@ public class ExpenseManagerApplication {
 		ServletRegistrationBean registration = new ServletRegistrationBean(new org.h2.server.web.WebServlet());
 		registration.addUrlMappings("/console/*");
 		return registration;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource bundleMessageSource = new ResourceBundleMessageSource();
+		bundleMessageSource.setBasename("messages");
+		return bundleMessageSource;
+	}
+
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseManagerApplication.class, args);
--- src/main/java/com/deepakrohan/expense/controller/CategoryController.java
+++ src/main/java/com/deepakrohan/expense/controller/CategoryController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,11 +16,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Locale;
 import java.util.zip.DataFormatException;
 
 @RestController
@@ -27,9 +30,16 @@ import java.util.zip.DataFormatException;
 @Slf4j
 public class CategoryController {
 
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/hello")
+    public String getMessage(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("morning.message", null, locale);
+    }
 
     @GetMapping("/categories")
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
